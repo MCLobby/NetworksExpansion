e8bb6ce071434567f2fd20adfa14c10d9c55500d
LobbyTech <1187755698@qq.com>
2025-12-06 06:16:45 +0800

    Update AdvancedAutoCraftingTable.java

--- src/main/java/com/ytdd9527/networksexpansion/implementation/machines/autocrafters/advanced/AdvancedAutoCraftingTable.java
+++ src/main/java/com/ytdd9527/networksexpansion/implementation/machines/autocrafters/advanced/AdvancedAutoCraftingTable.java
@@ -1,17 +1,16 @@
 package com.ytdd9527.networksexpansion.implementation.machines.autocrafters.advanced;
 
-import com.balugaq.netex.api.enums.CraftType;
-import com.ytdd9527.networksexpansion.core.items.machines.AbstractAdvancedAutoCrafter;
-import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
-import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
-import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import java.util.Map;
-import java.util.Set;
+import com.balugaq.netex.api.enums.CraftType;
+import com.ytdd9527.networksexpansion.core.items.machines.AbstractAdvancedAutoCrafter;
+
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 
 @EnableAsync
 public class AdvancedAutoCraftingTable extends AbstractAdvancedAutoCrafter {
